fe75aaab5ca28e25f22ee4163c6cdd8a889ce70c
sfsajid91 <sfsajid91@gmail.com>
2024-07-26 00:54:42 +0600

    feat: changed colors

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -27,45 +27,33 @@ const cLogger = ({ message, type }: Logger) => {
     const colorizedLevel = color(type.toUpperCase().padEnd(8));
     console.log(
         chalk.cyan(
-            `${orange(new Date().toISOString())} | ${chalk.bold(colorizedLevel)}  | ${message}`
+            `${orange(new Date().toISOString())} | ${chalk.bold(colorizedLevel)}  | ${orange(message)}`
         )
     );
 };
 
 export const logger = {
     info: (message: string, from?: string) => {
-        const coloredMessage = chalk.blue(
-            `${message}${from ? ` | from ${from}` : ''}`
-        );
         cLogger({
-            message: coloredMessage,
+            message: `${message}${from ? ` | from ${from}` : ''}`,
             type: 'info',
         });
     },
     error: (message: string, from?: string) => {
-        const coloredMessage = chalk.red(
-            `${message}${from ? ` | from ${from}` : ''}`
-        );
         cLogger({
-            message: coloredMessage,
+            message: `${message}${from ? ` | from ${from}` : ''}`,
             type: 'error',
         });
     },
     success: (message: string, from?: string) => {
-        const coloredMessage = chalk.green(
-            `${message}${from ? ` | from ${from}` : ''}`
-        );
         cLogger({
-            message: coloredMessage,
+            message: `${message}${from ? ` | from ${from}` : ''}`,
             type: 'success',
         });
     },
     warn: (message: string, from?: string) => {
-        const coloredMessage = chalk.yellow(
-            `${message}${from ? ` | from ${from}` : ''}`
-        );
         cLogger({
-            message: coloredMessage,
+            message: `${message}${from ? ` | from ${from}` : ''}`,
             type: 'warn',
         });
     },
