acc192bb875cb072f4c631d4a8a1ed28bd21b216
sfsajid91 <sfsajid91@gmail.com>
2024-08-19 11:19:15 +0600

    feat: added custom time and date in logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,6 +5,22 @@ type Logger = {
     type: 'info' | 'error' | 'success' | 'warn';
 };
 
+const getCurrentTime = () => {
+    const now = new Date();
+
+    return now
+        .toLocaleString('en-CA', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false, // Use 24-hour format
+        })
+        .replace(',', '');
+};
+
 const orange = chalk.hex('#FFC500');
 
 const cLogger = ({ message, type }: Logger) => {
@@ -27,7 +43,7 @@ const cLogger = ({ message, type }: Logger) => {
     const colorizedLevel = color(type.toUpperCase().padEnd(8));
     console.log(
         chalk.cyan(
-            `${orange(new Date().toISOString())} | ${chalk.bold(colorizedLevel)}  | ${orange(message)}`
+            `${orange(getCurrentTime())} | ${chalk.bold(colorizedLevel)}  | ${orange(message)}`
         )
     );
 };
